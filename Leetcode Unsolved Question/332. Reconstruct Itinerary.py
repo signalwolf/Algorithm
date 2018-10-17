bc866af4318aeba8de37995eec606fb51cc743f2
signalwolf <XuHanSD@gmail.com>
2018-10-16 22:15:10 -0700

    check in

---
+++ Leetcode Unsolved Question/332. Reconstruct Itinerary.py
@@ -0,0 +1,9 @@
+
+# sounds like an topological sort, because you must go to one city then you can move to another city,
+# shouldn't normal graph can resolve the problem? Greedy select the smallest order node.
+
+def findItinerary(tickets):
+    """
+    :type tickets: List[List[str]]
+    :rtype: List[str]
+    """
