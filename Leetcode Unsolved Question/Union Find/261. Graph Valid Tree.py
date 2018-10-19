e339d069d5891071984d143fc71009d8488c999c
signalwolf <XuHanSD@gmail.com>
2018-10-19 11:16:00 -0700

    Update Leetcode Unsolved Question/Union Find/261. Graph Valid Tree.py

--- Leetcode Unsolved Question/Union Find/261. Graph Valid Tree.py
+++ Leetcode Unsolved Question/Union Find/261. Graph Valid Tree.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# 没有使用任何的
+# 没有使用任何的optimization.
 
 class Solution(object):
 
@@ -20,5 +20,32 @@ class Solution(object):
         for edge in edges:
             p, q = self.root(edge[0], ids), self.root(edge[1], ids)
             if p == q: return False
+            ids[p] = q
+        return True
+
+# 使用了所有的optimization
+class Solution(object):
+
+    def root(self, node, ids):
+        while node != ids[node]:
+            ids[node] = ids[ids[node]]
+            node = ids[node]
+        return node
+
+    def validTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+        if len(edges) != n - 1: return False
+        ids = range(n)
+        size = [1] * n
+        for edge in edges:
+            p, q = self.root(edge[0], ids), self.root(edge[1], ids)
+            if p == q: return False
+            if size[p] < size[q]:
+                p, q = q, p
+            size[q] += size[p]
             ids[p] = q
         return True
