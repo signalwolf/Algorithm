61ff88ae9dcf5f79e2125012b37d7644ce8317cd
signalwolf <XuHanSD@gmail.com>
2018-10-21 10:29:28 -0700

    Update Data_structure/Trie.py

--- Data_structure/Trie.py
+++ Data_structure/Trie.py
@@ -1,6 +1,7 @@
 class Tries(object):
     def __init__(self):
         self.tries = {}
+        self.counter = 0
 
     def make_tries(self, wordList):
         for i, word in enumerate(wordList):
@@ -9,6 +10,7 @@ class Tries(object):
                 curr_dict = curr_dict.setdefault(c, {})
             else:
                 curr_dict['end'] = i
+            self.counter += 1
 
     def search(self, word):
         curr_dict = self.tries
@@ -18,11 +20,25 @@ class Tries(object):
             else:
                 return False
         else:
-            
+            if 'end' in curr_dict:
+                return curr_dict['end']
+            else:
+                return False
+
+    def insert(self, word):
+        curr_dict = self.tries
+        for c in word:
+            curr_dict = curr_dict.setdefault(c, {})
+        else:
+            curr_dict['end'] = self.counter
+            self.counter += 1
+
 
 
 
 wordList = ['han', 'xu', 'fight']
 tries = Tries()
 tries.make_tries(wordList)
+tries.insert('hand')
+tries.insert('hands')
 print tries.tries
