33cb51420dcdf95c9e57d678e625ae8a1f8deaa7
signalwolf <XuHanSD@gmail.com>
2018-10-20 20:22:22 -0700

    Update Data_structure/Graph.py

---
+++ Data_structure/Graph.py
@@ -0,0 +1,118 @@
+
+# My implementation for directed graph
+from collections import defaultdict
+class Graph(object):
+    def __init__(self):
+        self.graph = {}
+
+    def addVertex(self, vertex):
+        self.graph[vertex] = defaultdict(int)
+
+    def addEdge(self, v1, v2, weight):
+        if v1 not in self.graph:
+            self.addVertex(v1)
+
+        if v2 not in self.graph:
+            self.addVertex(v2)
+
+        self.graph[v1][v2] = weight
+        # self.graph[v2][v1] = weight
+
+    def getVertex(self, v):
+        if v in self.graph.keys():
+            return self.graph[v]
+        else:
+            return None
+
+    def getVertices(self):
+        return self.graph.keys()
+
+# Books implementation:
+class Vertex:
+    def __init__(self,key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self,nbr,weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
+
+class Graph2:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self,key):
+        self.numVertices = self.numVertices + 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self,n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def __contains__(self,n):
+        return n in self.vertList
+
+    def addEdge(self,f,t,cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+    def getVertices(self):
+        return self.vertList.keys()
+
+    def __iter__(self):
+        return iter(self.vertList.values())
+    
+g = Graph2()
+for i in range(6):
+    g.addVertex(i)
+g.vertList
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+for v in g:
+    for w in v.getConnections():
+        print("( %s , %s )" % (v.getId(), w.getId()))
+
+
+print 'My implementation'
+g = Graph()
+for i in xrange(6):
+    g.addVertex(i)
+
+g.addEdge(0,1,5)
+g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,8)
+g.addEdge(5,2,1)
+for v in g.graph.keys():
+    for w in g.graph[v]:
+        print("( %s , %s )" % (v, w))
