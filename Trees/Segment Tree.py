a9f645863856ccf6b09a1a27aaf6dc66455607f2
signalwolf <XuHanSD@gmail.com>
2018-10-22 16:08:03 -0700

    Update Trees/Segment Tree.py

--- Trees/Segment Tree.py
+++ Trees/Segment Tree.py
@@ -131,7 +131,7 @@ class NumArray2(object):
         """
 
         def buildSegementTree(nums, start, end):
-            if start > end: return None
+            # if start > end: return None
             if start == end:
                 return Treenode(start, end, nums[start])
             root = Treenode(start, end)
@@ -152,6 +152,21 @@ class NumArray2(object):
         :type val: int
         :rtype: void
         """
+        def update_tree (root):
+            if root.start == root.end == i:
+                prev = root.sums
+                root.sums = val
+                return val - prev
+
+            mid = root.start + (root.end - root.start) / 2
+            if mid >= i:
+                diff = update_tree(root.left)
+            else:
+                diff = update_tree(root.right)
+            root.sums += diff
+            return diff
+        update_tree(self.root)
+
 
     def sumRange(self, i, j):
         """
@@ -159,14 +174,27 @@ class NumArray2(object):
         :type j: int
         :rtype: int
         """
+        def rangesum(root, i, j):
+            if i == root.start and j == root.end:
+                return root.sums
+
+            mid = root.start + (root.end - root.start)/2
+            if j <= mid:
+                return rangesum(root.left, i, j)
+            elif i > mid:
+                return rangesum(root.right, i, j)
+            else:
+                return rangesum(root.left, i, mid) + rangesum(root.right, mid + 1, j)
+        return rangesum(self.root, i, j)
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # obj.update(i,val)
 # param_2 = obj.sumRange(i,j)
-nums = range(100)
+nums = range(10)
 print sum(nums)
 arr1 = NumArray(nums)
 arr2 = NumArray2(nums)
-print arr1.root.total
-print arr2.root.sums
+
+print arr2.sumRange(4,8)
+print arr1.sumRange(4,8)
