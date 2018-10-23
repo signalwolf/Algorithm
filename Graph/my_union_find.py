06769688e37185e84fbfc4e0bb0e7f0a9331e01b
signalwolf <XuHanSD@gmail.com>
2018-10-23 08:59:03 -0700

    Update Graph/my_union_find.py

--- Graph/my_union_find.py
+++ Graph/my_union_find.py
@@ -25,11 +25,37 @@ g.addEdge(2,4)
 g.addEdge(3,5)
 g.addEdge(3,6)
 
-def union_find(graph, n):
-    visited = [False] * n
+def root(ids, v, size):
+    if v not in ids:
+        ids[v] = v
+        size[v] = 1
+    while v != ids[v]:
+        ids[v] = ids[ids[v]]
+        v = ids[v]
+    return v
 
+def union_find(graph):
+    ids = {}
+    size = {}
+    #count = 0
+    for v in graph:
+        vr = root(ids, v, size)
+        for ngb in graph[v]:
+            ngb_root = root(ids, ngb, size)
+            if vr == ngb_root: continue
+
+            if size[vr] < size[ngb_root]:
+                vr, ngb_root = ngb_root, vr
+
+            ids[vr] = ngb_root
+            size[ngb_root] += size[vr]
+    return ids, size
 
 
 
 print g.graph
-# print union_find(g.graph, 7)
+ids, size =  union_find(g.graph)
+for id in ids:
+    ids[id] = root(ids, id, size)
+print ids
+
