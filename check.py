b1e4c446200589754e988247b67f8cc188ec0bd6
signalwolf <XuHanSD@gmail.com>
2019-01-01 12:04:41 -0800

    update

--- check.py
+++ check.py
@@ -1,3 +1,10 @@
-tmp = [1,2,3]
-for index, value in enumerate(tmp):
-    print index, value
+from hashlib import md5
+val = '10'
+m = md5(val)
+print m
+tmp = m.hexdigest()
+print tmp
+print type(tmp), len(tmp)
+print int(tmp, 16)
+print int('10')
+print int('10', 10)
