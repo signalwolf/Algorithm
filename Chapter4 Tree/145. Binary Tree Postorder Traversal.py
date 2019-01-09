d52f49b2187896ba4e289c0923729b9863e4f101
signalwolf <XuHanSD@gmail.com>
2019-01-08 22:43:05 -0800

    updates

--- Chapter4 Tree/145. Binary Tree Postorder Traversal.py
+++ Chapter4 Tree/145. Binary Tree Postorder Traversal.py
@@ -13,3 +13,28 @@ class Solution(object):
         """
         if not root: return []
         return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        res, stack = [], [root]
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.left:
+                stack.append(curr.left)
+            if curr.right:
+                stack.append(curr.right)
+        return res[::-1]
